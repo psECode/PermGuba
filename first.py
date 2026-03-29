6dcc1c7a4bf80f11b6290fc6c0da64c1e30ad16c
psECode <n1kitagyba@yandex.ru>
2026-03-29 20:57:15 +0500

    test1

--- first.py
+++ first.py
@@ -2,6 +2,7 @@ def main():
     print('My first git-repo')
     print("also did this for check")
     print("second check")
+    print("adding new branch to then deliver a pr")
 
 
 if __name__ == '__main__':
